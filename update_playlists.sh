3dc7e6217a2cf40f12bcfbe19fa52ae84abe88be
Adelaido Javier Fernández Cano <adelaido.fernandez@avoristravel.com>
2026-01-23 14:53:01 +0100

    update update_playlists

--- update_playlists.sh
+++ update_playlists.sh
@@ -7,25 +7,32 @@ if [[ `ip route list dev $ETH_DEVICE` =~ src\ ([0-9.]+) ]]; then
 else
   export ETH_IPV4="127.0.0.1"
 fi
+HOST=$ETH_IPV4:5004
 
-(timeout 1 bash -c '</dev/tcp/$ETH_IPV4/5004') 2> /dev/null || (echo "Port for livestream-companion isn't reachable"; exit 1)
+if ! (timeout 1 bash -c '</dev/tcp/$ETH_IPV4/5004') 2> /dev/null; then
+    echo "$HOST not reachable"
+    exit 1
+fi
 
-echo "Using gateway IP $ETH_IPV4 on device $ETH_DEVICE"
+echo "Using gateway host $HOST found on $ETH_DEVICE"
 
-curl -s "http://$ETH_IPV4:5004/api/playlists" | jq -c .[] | while read -r obj; do
+curl -s "http://$HOST/api/playlists" | jq -c .[] | while read -r obj; do
     id=$(echo "$obj" | jq -r '.ID')    
     status=$(echo "$obj" | jq -r '.ImportStatus')
     [ "$status" == "1" ] && echo "Already importing playlist $id" && continue
-    echo "Updating playlist with ID: $id"
-    curl -s -X PUT "http://$ETH_IPV4:5004/api/playlist/$id" \
+    echo -en "\nUpdating playlist with ID: $id "
+    curl -s -X PUT "http://$HOST/api/playlist/$id" \
         -H "Content-Type: application/json" \
         -d "$obj" > /dev/null
     while [ $status -ne 1 ]; do
-      status=`curl -s "http://$ETH_IPV4:5004/api/playlist/3" | jq -r .ImportStatus`
+      status=`curl -s "http://$HOST/api/playlist/$id" | jq -r .ImportStatus`
       sleep 5
     done
     while [ $status -eq 1 ]; do
-      status=`curl -s "http://$ETH_IPV4:5004/api/playlist/3" | jq -r .ImportStatus`
+      status=`curl -s "http://$HOST/api/playlist/$id" | jq -r .ImportStatus`
       sleep 10
+      echo -n '.'
     done
 done
+
+echo -e "\ndone"
